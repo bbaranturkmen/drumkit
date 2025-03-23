94b8ef9c34cb70f98e4ca0160e88c24ce3a40dce
baran türkmen <baranturkmenn@gmail.com>
2025-03-23 15:00:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+document.getElementById("a").addEventListener("click",function(){
+    const audio = new Audio("drum/boom.wav")
+    audio.play();
+});
+
+document.getElementById("s").addEventListener("click",function(){
+    const audio = new Audio("drum/clap.wav")
+    audio.play();
+});
+
+document.getElementById("d").addEventListener("click",function(){
+    const audio = new Audio("drum/hihat.wav")
+    audio.play();
+});
+
+document.getElementById("f").addEventListener("click",function(){
+    const audio = new Audio("drum/kick.wav")
+    audio.play();
+});
+
+document.getElementById("g").addEventListener("click",function(){
+    const audio = new Audio("drum/openhat.wav")
+    audio.play();
+});
+
+document.getElementById("h").addEventListener("click",function(){
+    const audio = new Audio("drum/ride.wav")
+    audio.play();
+});
+
+document.getElementById("j").addEventListener("click",function(){
+    const audio = new Audio("drum/tom.wav")
+    audio.play();
+});
+
+document.getElementById("k").addEventListener("click",function(){
+    const audio = new Audio("drum/snare.wav")
+    audio.play();
+});
+
+document.getElementById("l").addEventListener("click",function(){
+    const audio = new Audio("drum/tink.wav")
+    audio.play();
+});
+
+
+
+
